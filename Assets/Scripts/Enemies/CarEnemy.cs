8c482bb3974deaae7e0920b078e465253ff3a8ab
alexianeuf <neufond.alexia@gmail.com>
2020-02-29 00:29:24 -0500

    Add sounds

--- Assets/Scripts/Enemies/CarEnemy.cs
+++ Assets/Scripts/Enemies/CarEnemy.cs
@@ -11,41 +11,59 @@ namespace Enemies
 
         [SerializeField] [Tooltip("Speed when charge the player")]
         private float _chargeSpeed = 5.0f;
-        
+
+        [SerializeField] [Tooltip("Sound launch when hit the player")]
+        private AudioClip _collidSound;
+
         protected override void AttackPlayer()
         {
             // if speed is not the chargeSpeed
             if (!hasCharged && Math.Abs(_chargeSpeed - _speed) > 0.01)
             {
                 // Set the attack speed
-                isAttacking = true;
+                IsAttacking = true;
                 previousSpeed = _speed;
                 _speed = _chargeSpeed;
             }
-            else if(Vector3.Distance(player.position, transform.position) > _maxAttackDistance)
+            else if (Vector3.Distance(Player.position, transform.position) > _maxAttackDistance)
             {
                 // The enemy goes back to its initial speed
                 _speed = previousSpeed;
                 previousSpeed = _chargeSpeed;
-                isAttacking = false;
+                IsAttacking = false;
             }
         }
 
+
         private void OnTriggerEnter(Collider other)
         {
-            StartCoroutine(ReduceSpeed());
+            if (other.CompareTag("Player"))
+            {
+                AudioSource.PlayClipAtPoint(_collidSound, other.transform.position, 1.0f);
+                GameObject player = other.transform.parent.gameObject;
+                PlayerHealthController playerHealthController = player.GetComponent<PlayerHealthController>();
+
+                if (playerHealthController != null)
+                    playerHealthController.Damage(_damage);
+
+                NavMeshAgent.SetDestination(transform.position);
+
+                StartCoroutine(ReduceSpeed());
+            }
+
             // Damage the player
         }
 
         private IEnumerator ReduceSpeed()
         {
             hasCharged = true;
-            _speed = previousSpeed / 2.0f;
+            if (previousSpeed > 0)
+                _speed = previousSpeed / 2.0f;
             yield return new WaitForSeconds(2f);
             _speed = previousSpeed;
             previousSpeed = _chargeSpeed;
             hasCharged = false;
-            isAttacking = false;
+            IsAttacking = false;
         }
     }
 }
---
+++ Assets/Scripts/Items/Medikit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Items
+{
+    public class Medikit : MonoBehaviour
+    {
+        [SerializeField] [Tooltip("Percent of health to give back")] [Range(20, 100)]
+        private float _healingPercent;
+
+        [SerializeField] [Tooltip("Sound launch when item is collect")]
+        private AudioClip _pickUpItemSound;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                GameObject player = other.transform.parent.gameObject;
+                PlayerHealthController playerHealthController = player.GetComponent<PlayerHealthController>();
+                AudioSource.PlayClipAtPoint(_pickUpItemSound, transform.position, 200);
+
+                if (playerHealthController != null)
+                    playerHealthController.Heal(_healingPercent);    
+                
+                Destroy(gameObject);
+            }
+        }
+    }
+}
