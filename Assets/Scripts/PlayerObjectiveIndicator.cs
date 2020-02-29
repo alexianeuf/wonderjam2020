d4cb3dd2689665819c2d122783e1700e4b86a093
Elercia <rabierv@hotmail.fr>
2020-02-29 00:16:45 -0500

    Objectives

---
+++ Assets/Scripts/PlayerObjectiveIndicator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObjectiveIndicator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var objective = GameManager.instance.GetComponent<ObjectiveController>().m_currentObjective;
+        if (objective == null)
+            return;
+        
+        transform.LookAt(objective.transform);
+    }
+}
