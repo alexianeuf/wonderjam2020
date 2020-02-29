5bf951d444a7d459729f0dd04c50f7ef5c5539cb
alexianeuf <neufond.alexia@gmail.com>
2020-02-28 22:52:57 -0500

    Player health

--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -1,7 +1,26 @@
-﻿namespace DefaultNamespace
+﻿using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
 {
-    public class PlayerHealthController
+    [SerializeField] [Tooltip("Player max health")]
+    private float _maxHealth = 100.0f;
+
+    [SerializeField] // TODO : remove this Serialize
+    private float currentHealth;
+
+    private void Awake()
     {
-        
+        currentHealth = _maxHealth;
+    }
+
+    public void Damage(float damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth < 0)
+        {
+            // TODO : launch GameOver Screen
+            Destroy(gameObject);
+        }
     }
 }
