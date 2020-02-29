c14acc6cab083ff92cb483cf90e1a5b0f15f28db
Benoît RICHARD <btrd29@hotmail.fr>
2020-02-29 15:23:20 -0500

    les pietons ils marchent dans la ville et ils vont même aux putes

--- Assets/Scripts/Pedestrians/WayPoint.cs
+++ Assets/Scripts/Pedestrians/WayPoint.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class WayPoint : MonoBehaviour
 {
+    private void Awake()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         other.GetComponent<Pedestrian>().OnWayPointEnter(this.gameObject);
