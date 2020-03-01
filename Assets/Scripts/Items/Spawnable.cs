57ec321672420717e2e305397d2db565cb3a220e
Benoît RICHARD <btrd29@hotmail.fr>
2020-02-29 23:54:30 -0500

    update item, itemmanager, waypoint color and more

---
+++ Assets/Scripts/Items/Spawnable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Spawnable
+{
+    [System.NonSerialized]
+    private string m_name;
+
+    public GameObject _prefab;
+
+    public float _spawnFrequence = 5f;
+    public float _maxNumber = 3f;
+    public float _currentNumber = 0f;
+
+    public void SetName(string name)
+    {
+        m_name = name;
+    }
+
+    public string GetName()
+    {
+        return m_name;
+    }
+}
