6a29d340c38549a8d66cc2c1bfd975876d3bc7e9
Elercia <rabierv@hotmail.fr>
2020-02-29 19:48:10 -0500

    tag player for objectives

--- Assets/Scripts/Objective.cs
+++ Assets/Scripts/Objective.cs
@@ -3,15 +3,16 @@
 [RequireComponent(typeof(BoxCollider))]
 public class Objective : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject m_indicator;
+    [SerializeField] private GameObject m_indicator;
+
     public void TargetObjective(bool active)
     {
         m_indicator.SetActive(active);
     }
-    
+
     private void OnTriggerEnter(Collider other)
     {
-        GameManager.instance.GetComponent<ObjectiveController>().OnObjectiveEnter(this.gameObject);
+        if (other.CompareTag("Player"))
+            GameManager.instance.GetComponent<ObjectiveController>().OnObjectiveEnter(this.gameObject);
     }
 }
